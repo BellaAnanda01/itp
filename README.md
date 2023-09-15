f98bf5479cdbb221feba78800931653329893441
BellaAnanda01 <bella.ananda.ba011102@gmail.com>
2023-09-16 01:14:21 +0700

    Penjabaran detail yang perlu dikerjakan

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
-# About develop/merch
-Develop/merch merupakan bagian dari pengembangan website ITP yang berfokus pada bagian merchandise
+# About develop/ticket
+Develop/ticket merupakan bagian dari pengembangan website ITP yang berfokus pada bagian merchandise
 
 ## Yang Sudah Selesai
-1. Sudah bisa satu baris outputnya
-2. Bisa buka detail untuk setiap produk
-3. Bisa mendapatkan option
-4. Bisa mendapatkan hasil dari pilihan pada option
+-
 
 ## Yang Belum Selesai
-1. Belum membuat total pembelian
-2. Belum memunculkan apa saja yang dibeli di checkout
+1. 
 
 ## Masalah Yang Perlu Diselesaikan
-1. Bagaimana jika yang dibeli bukan baju?
---> Solusi: Membuat select option dan functionnya sesuai dengan produknya ATAU link ke beda tempat untuk masing2 produk yang punya optionnya sendiri
-2. TotalKeseluruhan ada 2 di email, yang pakai rp dan tidak
---> Solusi: Usahakan supaya dari values nya bisa diedit untuk total keseluruhan pakai rp semua
-3. Bubblewrap true or false belum keluar di sheets
---> Solusi: Belum menemukan solusi
+1. 
--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ import {
 } from "react-router-dom";
 import Home from './pages/Home';
 import Ticket from './pages/BuyTicket';
-import Products from './pages/Products';
-import CheckOut2 from './pages/CheckOut2';
 import Test from './pages/Test';
 import TicketTypes from './pages/TicketTypes';
 import TicketSuccess from './pages/TicketSuccess';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import MerchSuccess from './pages/MerchSuccess';
-import Payment from './pages/Payment';
-import CheckOut from './pages/CheckOut';
-import Products2 from './pages/Products2';
-import Product from './pages/Product';
 
 function App() {
   return (
@@ -29,14 +22,7 @@ function App() {
         <Route path="/tickettypes" element={<TicketTypes />} />
         <Route path="/buyticket" element={<Ticket />} />
         <Route path="/ticketsuccess" element={<TicketSuccess />} />
-        <Route path="/merchsuccess" element={<MerchSuccess />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products2" element={<Products2 />} />
-        <Route path="/payment" element={<Payment />} />
         <Route path="/test" element={<Test />} />
-        <Route path="/checkout" element={<CheckOut2 />} />
-        <Route path="/checkout2" element={<CheckOut />} />
-        <Route path="/product/:id" element={<Product />} />
       </Routes>
       <Footer />
     </BrowserRouter>
