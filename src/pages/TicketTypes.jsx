cfd44e6f881b047e2887fdf3f4e7f6fd582fb9c2
BellaAnanda01 <81849304+BellaAnanda01@users.noreply.github.com>
2023-10-23 17:11:23 +0700

    Update TicketTypes.jsx

--- src/pages/TicketTypes.jsx
+++ src/pages/TicketTypes.jsx
@@ -120,16 +120,16 @@ const TicketTypes = () => {
                             <SecondDivTicketsH style={{color: "#2f6eed"}}>Open House</SecondDivTicketsH>
                             <SecondDivTicketsP style={{marginBottom: 0}}>Sabtu, 11 November 2023</SecondDivTicketsP>
                             <b><SecondDivTicketsP style={{marginTop: 0}}>at Fakultas Psikologi UI</SecondDivTicketsP></b>
-                            <SecondDivTicketsP style={{margin: "10px 0 0"}}>Presale 2:</SecondDivTicketsP>
-                            <SecondDivTicketsP style={{margin: "0 0 15px"}}>Rp45.000</SecondDivTicketsP>
+                            <SecondDivTicketsP style={{margin: "10px 0 0"}}>Normal Price:</SecondDivTicketsP>
+                            <SecondDivTicketsP style={{margin: "0 0 15px"}}>Rp55.000</SecondDivTicketsP>
                             <button onClick={(e) => buttonValue(e, 'Offline')} style={{fontFamily: "'Sniglet', cursive", textAlign: "center", margin: 0, backgroundColor: "#ff4747", color: "white", padding: "8px", borderRadius: "8px"}}>Get Your Tickets Now!</button>
                         </SecondDivTicketsBackground>
                         <SecondDivTicketsBackground>
                             <SecondDivTicketsH style={{color: "#2f6eed"}}>Open House</SecondDivTicketsH>
                             <SecondDivTicketsP style={{marginBottom: 0}}>Sabtu, 11 November 2023</SecondDivTicketsP>
                             <b><SecondDivTicketsP style={{marginTop: 0}}>via Zoom Meeting</SecondDivTicketsP></b>
-                            <SecondDivTicketsP style={{margin: "10px 0 0"}}>Presale 2:</SecondDivTicketsP>
-                            <SecondDivTicketsP style={{margin: "0 0 15px"}}>Rp30.000</SecondDivTicketsP>
+                            <SecondDivTicketsP style={{margin: "10px 0 0"}}>Normal Price:</SecondDivTicketsP>
+                            <SecondDivTicketsP style={{margin: "0 0 15px"}}>Rp37.000</SecondDivTicketsP>
                             <button onClick={(e) => buttonValue(e, 'Online')} style={{fontFamily: "'Sniglet', cursive", textAlign: "center", margin: 0, backgroundColor: "#ff4747", color: "white", padding: "8px", borderRadius: "8px"}}>Get Your Tickets Now!</button>
                         </SecondDivTicketsBackground>
                     </SecondDivTickets>
